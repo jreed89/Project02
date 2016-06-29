3b121c4415be2d1ca7e4023b69049deb245e38ed
Joshua Daniel <jrdaniel@ncsu.edu>
2016-06-29 18:55:19 -0400

    uncomment array

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -10,9 +10,9 @@ window.onload = function(){
 //this is where all of my "universal variables" will go, access them when needed 
 var arrayX = []
 var arrayO = []
-// winCombos  = ['1', '2', '3', 
-				 // '4', '5', '6', 
-				 // '7', '8', '9']
+winCombos  = ['1', '2', '3', 
+				 '4', '5', '6', 
+				 '7', '8', '9']
 // winCombos = [ ["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], 
 // 			  ["1", "4", "7"], ["2", "5", "8"], ["3", "6", "9"], 
 // 			  ["1", "5", "9"], ["3", "5", "7"]]
